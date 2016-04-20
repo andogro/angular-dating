bd75ec1c90e3b0aa791b53777e30ab310b410e50
Andy Gross <andogro@gmail.com>
2016-04-19 19:10:15 -0600

    added registration and login page styling

--- src/js/controllers/registerController.js
+++ src/js/controllers/registerController.js
@@ -2,12 +2,7 @@
 app.controller('registerController', ['$scope', '$http', 'NameService', 
     function($scope, $http, NameService) {
 
-        $scope.loading = true;
 
-        NameService.search().then(function (data) {
-        $scope.info = data.data.data;
-        $scope.loading = false;
-        });
 
 }]);
 
